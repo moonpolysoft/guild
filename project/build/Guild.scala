aec7c415dbd640a04eaa84f71189cbfdbd2acada
Cliff Moon <cliff@moonpolysoft.com>
2010-07-15 14:50:30 -0700

    need jetlang repository

--- project/build/Guild.scala
+++ project/build/Guild.scala
@@ -27,6 +27,7 @@ class Guild(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with postero
   /**
    * Repositories
    */
+  val jetlangRepo = "Jet Lang Repository" at "http://jetlang.googlecode.com/svn/repo/"
   val scalaToolsSnapshots = "scala-tools.org Snapshots" at "http://scala-tools.org/repo-snapshots"
 
   /**
@@ -39,6 +40,7 @@ class Guild(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with postero
    */
   val scalaTest = buildScalaVersion match {
     case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
     case "2.8.0.RC6" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test" withSources() intransitive()
     case unknown => error("no known scalatest impl for %s".format(unknown))
   }

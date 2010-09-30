787eb260d0aa66d591dca2055bdf8ffc44fcbdb8
cliff <cliff@moonpolysoft.com>
2010-09-30 11:46:09 -0400

    bump

--- project/build/Guild.scala
+++ project/build/Guild.scala
@@ -1,3 +1,6 @@
+import sbt._
+import sbt.StringUtilities._
+
 class Guild(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with posterous.Publish {
   override def compileOptions = super.compileOptions ++ List(Deprecation, Unchecked)
   
@@ -5,17 +8,17 @@ class Guild(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with postero
    * Home Repo
    */
   override def managedStyle = sbt.ManagedStyle.Maven
-  val publishTo = sbt.Resolver.file("Local Cache", ("." / "target" / "repo").asFile)
+  val publishTo = Resolver.file("gh-pages", new java.io.File("/Users/cliff/projects/guild-pages/repository"))
 
   /**
    * Publish to a local temp repo, then rsync the files over to repo.codahale.com.
    */
-  def publishToLocalRepoAction = super.publishAction
+/*  def publishToLocalRepoAction = super.publishAction
   override def publishAction = task {
     log.info("Uploading to repo.codahale.com")
     sbt.Process("rsync", "-avz" :: "target/repo/" :: "codahale.com:/home/codahale/repo.codahale.com" :: Nil) ! log
     None
-  } describedAs("what") dependsOn(test, publishToLocalRepoAction)
+  } describedAs("what") dependsOn(test, publishToLocalRepoAction)*/
 
   /**
    * Publish the source as well as the class files.
@@ -40,8 +43,9 @@ class Guild(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with postero
    */
   val scalaTest = buildScalaVersion match {
     case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC6" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC2"   => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC6"   => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0"       => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test" withSources() intransitive()
     case unknown => error("no known scalatest impl for %s".format(unknown))
   }
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
